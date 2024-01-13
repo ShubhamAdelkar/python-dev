4d238464eb2d5f713e408ef54b8b652e6461737b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-13 20:22:48 +0530

    simple calculator 🚀

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter a number: "))
+op = (input("Enter a operator: "))
+num2 = float(input("Enter another number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+   print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    "Invalid Operator"
+
+
